97394e3d54b1757801b9b6411aa8798aecb7489f
Giver225 <goverdovsk.da@gmail.com>
2023-11-06 01:20:43 +0000

    1. Fix footer 2. Add dark theme to languages 3. Fix big new section on mobiles 4. Some burger fixes 5. Add never want to miss element

--- public1/js/news_script.js
+++ public1/js/news_script.js
@@ -1,7 +1,7 @@
 let changeThemeButtons = document.querySelectorAll('.theme_slider label .slider'); // Помещаем кнопки смены темы в переменную
 let themeCheckbox = document.querySelectorAll('.theme_slider input[type=checkbox]');
 let selectTheme = document.querySelectorAll('.select_theme');
-
+let body = document.querySelector('body');
 
 themeCheckbox.forEach(button => {
     button.onclick =  function () { // К каждой добавляем обработчик событий на клик
@@ -19,7 +19,7 @@ themeCheckbox.forEach(button => {
         });
         themeCheckbox.forEach(button => {
             if (button !== clicked){
-                button.click()
+                button.checked = clicked.checked
             }
         })
          
@@ -33,7 +33,7 @@ selectTheme.forEach(select =>{
     select.onchange = function(){
         theme = this.options[this.selectedIndex].value;
         themeCheckbox.forEach(button =>{
-            button.click()
+            button.checked = !button.checked
         });
         applyTheme(theme);
     }
@@ -44,35 +44,43 @@ function applyTheme(themeName) {
     localStorage.setItem('theme', themeName);
 }
 
+
+
 let activeTheme = localStorage.getItem('theme'); // Проверяем есть ли в LocalStorage записано значение для 'theme' и присваиваем его переменной.
 
-if(activeTheme === null || activeTheme === 'dark') { // Если значение не записано, или оно равно 'light' - применяем светлую тему
+body.onload = function(){
     
-    applyTheme('dark');
-    themeCheckbox.forEach(button =>{
-        button.checked = 1;
-    });
-    selectTheme.forEach(select =>{
-        select.selectedIndex = 0
-    });
+    if(activeTheme === null || activeTheme === 'dark') { // Если значение не записано, или оно равно 'light' - применяем светлую тему
     
-
-} else if (activeTheme === 'light') { // Если значение равно 'dark' - применяем темную
-    applyTheme('light');
-    themeCheckbox.forEach(button =>{
-        button.checked = 0;
-    });
-    selectTheme.forEach(select =>{
-        select.selectedIndex = 1
-    });
+        applyTheme('dark');
+        themeCheckbox.forEach(button =>{
+            button.checked = 1;
+        });
+        selectTheme.forEach(select =>{
+            select.selectedIndex = 0
+        });
+        
+    
+    } else if (activeTheme === 'light') { // Если значение равно 'dark' - применяем темную
+        applyTheme('light');
+        themeCheckbox.forEach(button =>{
+            button.checked = 0;
+        });
+        selectTheme.forEach(select =>{
+            select.selectedIndex = 1
+        });
+    }
 }
 
 
+
 var toggle = document.querySelector('.hamburger');
+let header = document.querySelector('header')
   
   toggle.onclick = function(e) {
     this.classList.toggle('opened');
-    $(".mobile_menu").slideToggle()
+    $(".mobile_menu").slideToggle();
+    header.classList.toggle('header_opened')
     
   };
 
@@ -108,7 +116,7 @@ pages.forEach(page =>{
 
 let languages_btn = document.querySelector('.languages__active');
 let languages_links = document.querySelector('.languages__links');
-let body = document.querySelector('body');
+
 languages_btn.onclick = function(){
     languages_links.classList.toggle('show')
     // body.classList.toggle('body_languages_showed')
